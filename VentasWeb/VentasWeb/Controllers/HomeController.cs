2100f162e501a9b5987f8f95a2ec42740d46714c
Soluciones Cyberdine <70183721+cyberdine1976@users.noreply.github.com>
2021-09-13 20:33:27 +0200

    First commit

---
+++ VentasWeb/VentasWeb/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VentasWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+           
+
+           
+            return View();
+        }
+
+       
+
+    }
+}
---
+++ VentasWeb/VentasWeb/Helpers/Helpers.cs
@@ -0,0 +1,25 @@
+﻿using CapaDatos;
+using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VentasWeb.Helpers
+{
+    public static class Helpers
+    {
+
+        public static MvcHtmlString ActionLinkAllow(this HtmlHelper helper)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+
+
+            return new MvcHtmlString(sb.ToString());
+        }
+    }
+}
